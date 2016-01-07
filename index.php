a1a989e86960703063b25b65ebe098bee0d577d2
Jesse Shawl <jesse@jshawl.com>
2016-01-07 15:11:02 -0500

    add the loop

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
-index.pgp
+
+<?php
+// the loop
+
+if(have_posts()):
+  while(have_posts()):
+    the_post(); ?>
+    <h2>
+      <a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?>
+      </a>
+    </h2>
+    <?php the_content(); ?>
+  <?php endwhile;
+endif;
+
+?>
+
 <?php get_footer(); ?>
